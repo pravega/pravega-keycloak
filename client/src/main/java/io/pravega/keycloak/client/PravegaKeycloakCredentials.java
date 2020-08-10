b73753931c2f6294cd5e797f0221d19ef85b5fdf
farukm <omar.faruk.buet@gmail.com>
2020-08-10 13:36:32 -0700

    support direct injection of the keycloak JSON in the client configuratio

--- client/src/main/java/io/pravega/keycloak/client/PravegaKeycloakCredentials.java
+++ client/src/main/java/io/pravega/keycloak/client/PravegaKeycloakCredentials.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2019 Dell Inc., or its subsidiaries. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 
 package io.pravega.keycloak.client;
@@ -13,6 +13,12 @@ package io.pravega.keycloak.client;
 import io.pravega.client.stream.impl.Credentials;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.PosixFilePermissions;
 
 import static io.pravega.auth.AuthConstants.BEARER;
 
@@ -26,8 +32,15 @@ public class PravegaKeycloakCredentials implements Credentials {
 
     // The actual keycloak client won't be serialized.
     private transient KeycloakAuthzClient kc = null;
+    private String keycloakJsonString = null;
 
     public PravegaKeycloakCredentials() {
+        init();
+        LOG.info("Loaded Keycloak Credentials");
+    }
+
+    public PravegaKeycloakCredentials(String keycloakJsonString) {
+        this.keycloakJsonString = keycloakJsonString;
         init();
         LOG.info("Loaded Keycloak Credentials");
     }
@@ -45,7 +58,27 @@ public class PravegaKeycloakCredentials implements Credentials {
 
     private void init() {
         if (kc == null) {
-            kc = KeycloakAuthzClient.builder().build();
+            if (keycloakJsonString != null) {
+                try {
+                    // KeycloakAuthzClient requires a file. We write the authentication credentials to
+                    // a secure file and delete it immediately after initialization.
+                    final Path tempPath = Files.createTempFile("keycloak-", ".json",
+                            // Only user has permissions to the file.
+                            PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rw-------")));
+                    try {
+                        try (PrintWriter out = new PrintWriter(tempPath.toFile())) {
+                            out.println(keycloakJsonString);
+                        }
+                        kc = KeycloakAuthzClient.builder().withConfigFile(tempPath.toString()).build();
+                    } finally {
+                        Files.delete(tempPath);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                kc = KeycloakAuthzClient.builder().build();
+            }
         }
     }
 }
